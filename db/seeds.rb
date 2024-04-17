3156e9e67aa7adf25aec34f730808f0ee7e06e31
mahongri <mahongri1993@gmail.com>
2024-04-17 02:08:59 -0400

    add model controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+5.times do
+  Restaurant.create(name: Faker::Company.name, address: Faker::Address.full_address,category: ["chinese", "italian", "japanese", "french", "belgian"].sample,phone_number: Faker::PhoneNumber.phone_number)
+end
